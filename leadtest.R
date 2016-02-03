9e6df08db448c5fd2b1253e8b391415231d7e5ef
Gerhard Nachtmann <kpm.nachtmann@gmail.com>
2016-02-03 15:09:19 +0100

    added real life speedtest for shift()

--- leadtest.R
+++ leadtest.R
@@ -1,5 +1,5 @@
 ##### R-Script to speedup leading / lagging
-##### Gerhard Nachtmann 20150723
+##### Gerhard Nachtmann 20150723, 20160203
 library(dplyr)
 library(data.table)
 library(microbenchmark)
@@ -13,7 +13,7 @@ data
 d1 <- tbl_df(data)
 d2 <- tbl_dt(data)
 d3 <- copy(d2)
-### see also shift() in dt1.9.5
+### see also shift() in dt1.9.6
 
 ##### data.table 1.9.4
 ### lag
@@ -27,6 +27,20 @@ data
 ### clean
 data[, c("lead.value", "lag.value") := NULL]
 
+##### data.table 1.9.6
+### lag
+data[, lag.value := shift(value, n = 1, type = "lag"),
+     by = groups]
+data
+
+### lead
+data[, lead.value := shift(value, n = 1, type = "lead"),
+     by = groups]
+data
+
+### clean
+data[, c("lead.value", "lag.value") := NULL]
+
 ##### dplyr
 d1r <- d1 %>% group_by(groups) %>%
        mutate(lead.value = lead(value, 1))
@@ -34,14 +48,17 @@ d1r
 rm(d1r)
 
 
-microbenchmark(dt = data[, lead.value := c(value[-1], NA), by = groups],
-               ldf = d1r <- d1 %>% group_by(groups) %>%
+microbenchmark(ldf = d1r <- d1 %>% group_by(groups) %>%
                             mutate(lead.value = lead(value, 1)),
                ldt = d2r <- d2 %>% group_by(groups) %>%
                             mutate(lead.value = lead(value, 1)),
-               dt1 = d3[, lead.value := c(value[-1], NA), by = groups]
+               dt194 = data[, lead.value := c(value[-1], NA),
+                          by = groups],
+               dt196 = d3[, lead.value := shift(value, n = 1,
+                                           type = "lead"),
+                          by = groups]
                )
-### dt = fastest; interesting: ldt is much slower than ldf
+### dt196 = fastest; interesting: ldt is much slower than ldf
 data
 d1r
 d2r
@@ -49,3 +66,19 @@ d3
 all.equal(as.data.frame(data), as.data.frame(d1r))
 all.equal(as.data.frame(data), as.data.frame(d2r))
 all.equal(as.data.frame(data), as.data.frame(d3))
+
+### hmmm -- dt196 is much slower in real life
+set.seed(1)
+mg <- data.table(expand.grid(year = 2012:2016, id = 1:1000),
+                 value = rnorm(5000))
+mg
+
+microbenchmark(dt194 = mg[, l1 := c(value[-1], NA), by = .(id)],
+               dt196 = mg[, l2 := shift(value, n = 1,
+                                   type = "lead"), by = .(id)])
+## Unit: milliseconds
+##   expr      min        lq      mean    median       uq        max neval
+##  dt194  4.93735  5.236034  5.718654  5.623736  5.74395   9.555922   100
+##  dt196 83.92612 87.530404 91.700317 90.953947 91.43783 257.473242   100
+
+mg[, all.equal(l1, l2)]
